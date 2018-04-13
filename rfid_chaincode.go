2d67fe33a2c88c935a282b07ea2c2b5fa3f05139
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-04-13 10:59:48 +0700

    add init

--- rfid_chaincode.go
+++ rfid_chaincode.go
@@ -26,6 +26,18 @@ type Person struct {
 
 
 func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	 args := stub.GetStringArgs()
+    if len(args) != 2 {
+            return shim.Error("Incorrect arguments. Expecting a key and a value")
+    }
+
+    // Set up any variables or assets here by calling stub.PutState()
+
+    // We store the key and the value on the ledger
+    err := stub.PutState(args[0], []byte(args[1]))
+    if err != nil {
+            return shim.Error(fmt.Sprintf("Failed to create asset: %s", args[0]))
+    }
 	return shim.Success(nil)
 }
 
